f90cba46af0b5e6de5a7fa4e9f5d9a0965bf2596
parthi-n <parthiban.n@live.com>
2025-01-11 01:06:29 +0800

    fix: navbar bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,39 @@ export default function Navbar({ availablePoints, totalItems }) {
 						<img alt="" src={logo} className="h-8 w-auto" />
 					</Link>
 				</div>
-				<div className="hidden lg:flex lg:gap-x-4 font-medium uppercase  items-center">
-					<Link to="/" className="text-sm font-medium">Products</Link>
+				<div className=" lg:flex lg:gap-x-4 font-medium uppercase  items-center">
+					<div className="hidden lg:flex lg:gap-x-4 items-center">
+						<Link to="/" className="text-sm font-medium">
+							Products
+						</Link>
+						<div className="relative mx-1.5">
+							<Link to="/cart" className="text-sm font-medium">
+								Cart
+							</Link>
+							{totalItems > 0 && (
+								<span className="bg-blue-500 text-[10px] px-1.5 font-semibold min-w-[16px] h-4 flex items-center justify-center text-white rounded-full absolute -top-1 left-[90%]">
+									{totalItems}
+								</span>
+							)}
+						</div>
+						<Link to="/orders" className="text-sm font-medium">
+							Orders
+						</Link>
+					</div>
+
+					<div className="rounded-full border border-blue-500 bg-blue-600/10 text-blue-800 font-medium px-3 py-0.5 flex gap-1 items-center align-baseline ml-1.5">
+						<span className="text-xs font-semibold"> CITI PTS:</span>
+						<span className="font-semibold">{availablePoints}</span>
+					</div>
+				</div>
+			</nav>
+			<div className="fixed bottom-0 bg-white/60 w-full py-8 lg:hidden">
+				<div className="mx-auto flex  gap-x-6 items-center justify-center text-sm font-semibold uppercase">
+					<Link to="/" className="">
+						Products
+					</Link>
 					<div className="relative mx-1.5">
-						<Link to="/cart" className="text-sm font-medium">
+						<Link to="/cart" className="">
 							Cart
 						</Link>
 						{totalItems > 0 && (
@@ -24,14 +53,11 @@ export default function Navbar({ availablePoints, totalItems }) {
 							</span>
 						)}
 					</div>
-					<Link to="/orders" className="text-sm font-medium">Orders</Link>
-
-					<div className="rounded-full border border-blue-500 bg-blue-600/10 text-blue-800 font-medium px-3 py-0.5 flex gap-1 items-center align-baseline ml-1.5">
-						<span className="text-xs font-semibold"> CITI PTS:</span>
-						<span className="font-semibold">{availablePoints}</span>
-					</div>
+					<Link to="/orders" className="">
+						Orders
+					</Link>
 				</div>
-			</nav>
+			</div>
 		</header>
 	);
 }
